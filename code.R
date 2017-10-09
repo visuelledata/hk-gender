540ca37f1d287598920fac4704eecffedbccb5d9
Chris Peralta <chris99994@gmail.com>
2017-10-09 16:06:06 +0800

    Loading data

---
+++ code.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+
+pop <- read_csv("D:/rproj/hk-gender/preclean-excel.csv", 
+                                  na = '-',
+                                  col_types = cols(Australian = col_integer(), 
+                                                   Indonesian = col_integer(), 
+                                                   Japanese = col_integer(), 
+                                                   Nepalese = col_integer(), 
+                                                   Pakistani = col_integer(), 
+                                                   Thai = col_integer()))
