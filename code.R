ec2a6156dfa16fd22f967921cb3504b39a9c8203
Chris Peralta <chris99994@gmail.com>
2017-10-11 00:12:07 +0800

    General distributions of age by sex

--- code.R
+++ code.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(forcats)
 
 pop <- read_csv("D:/rproj/hk-gender/preclean-excel.csv", 
                                   na = '-',
@@ -9,3 +9,36 @@ pop <- read_csv("D:/rproj/hk-gender/preclean-excel.csv",
                                                    Nepalese = col_integer(), 
                                                    Pakistani = col_integer(), 
                                                    Thai = col_integer()))
+
+# To Do: clean graphs, population distribution HK, distribution by nationality, ...
+
+
+pop$age_range <- factor(pop$age_range, levels = unique(pop$age_range))
+
+pop <- pop %>% 
+  gather(chinese_hk_resident:Others, key = 'nationality', value = 'population') %>% 
+  mutate(prop = population / sum(population, na.rm = TRUE))
+
+
+pop %>%  
+  filter(age_range != '0 - 4' & age_range != '80 - 84' & age_range != '85+') %>%
+  group_by(age_range, sex, nationality) %>%
+  summarize(population = sum(population),
+            prop = sum(prop)) %>% 
+  ggplot() + 
+  geom_col(aes(x = age_range, y = prop, fill = sex), position = 'dodge') + 
+  facet_wrap(~nationality, scale = 'free_y') #add this as an interactive graph
+  
+  
+pop %>%  
+  filter(age_range != '0 - 4' & age_range != '80 - 84' & age_range != '85+') %>%
+  group_by(age_range, sex) %>% 
+  summarize(population = sum(population)) %>% 
+  mutate(prop = population / sum(population)) %>% 
+  ggplot() + 
+  geom_col(aes(x = age_range, y = prop, fill = sex), position = 'dodge') + 
+  scale_y_continuous(limits = c(0, 1)) + 
+  geom_hline(yintercept = .504, aes(color = 'Blue')) +
+  coord_flip()
+    
+
