236113b34006263dfdf50ecf386b899007a15888
Chris Peralta <chris99994@gmail.com>
2017-10-11 16:16:00 +0800

    Chart cleaning

--- code.R
+++ code.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(forcats)
+library(SciencesPo)
+library(scales)
 
 pop <- read_csv("D:/rproj/hk-gender/preclean-excel.csv", 
                                   na = '-',
@@ -36,9 +38,34 @@ pop %>%
   summarize(population = sum(population)) %>% 
   mutate(prop = population / sum(population)) %>% 
   ggplot() + 
-  geom_col(aes(x = age_range, y = prop, fill = sex), position = 'dodge') + 
-  scale_y_continuous(limits = c(0, 1)) + 
-  geom_hline(yintercept = .504, aes(color = 'Blue')) +
+  geom_col(aes(x = age_range, y = prop, fill = sex, width = .85), position = 'dodge') + 
+  geom_text(aes(x = age_range, y = prop, fill = sex, label = percent(prop)), position = position_dodge(width = .9), vjust = .2, hjust = 1.1,size = 3, color = 'Grey70') +
+  scale_y_continuous(limits = c(0, 1), expand = c(0,0)) + 
+  scale_fill_manual(values = c('Red', 'Blue')) +
+  geom_hline(yintercept = .504, color = 'Grey', alpha = .7) + 
+  theme_pub() +
+  no_gridlines() + 
+  no_x_axis() +
+  no_x_line() +
+  no_legend() +
+  no_ticks() +
   coord_flip()
-    
+  
 
+pop %>%  
+  filter(age_range != '0 - 4' & age_range != '80 - 84' & age_range != '85+') %>%
+  group_by(age_range, sex) %>% 
+  summarize(population = sum(population)) %>% 
+  mutate(prop = population / sum(population)) %>% 
+  ggplot() + 
+  geom_col(aes(x = age_range, y = prop, group = sex, fill = prop >= .504 & sex == 'female' & age_range != '75 - 79', width = .85), position = 'dodge') + 
+  scale_fill_manual(values = c('Grey', 'Grey', 'Blue', 'Red')) +
+  geom_text(aes(x = age_range, y = prop, fill = sex, label = percent(prop)), position = position_dodge(width = .9), vjust = .2, hjust = 1.1,size = 3, color = 'White') +
+  scale_y_continuous(limits = c(0, 1), expand = c(0,0)) + 
+  geom_hline(yintercept = .504, color = 'Grey', alpha = .7) + coord_flip()
+  theme_pub() +
+  no_gridlines() + 
+  no_x_axis() +
+  no_x_line() +
+  no_ticks() +
+  coord_flip()
