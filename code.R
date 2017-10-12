d2b5c9d525b43c3bc6c820444609c5169f63826d
Chris Peralta <chris99994@gmail.com>
2017-10-12 14:23:36 +0800

    Some annotation

--- code.R
+++ code.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(forcats)
 library(SciencesPo)
 library(scales)
+library(stringr)
 
 pop <- read_csv("D:/rproj/hk-gender/preclean-excel.csv", 
                                   na = '-',
@@ -32,32 +33,38 @@ pop %>%
   facet_wrap(~nationality, scale = 'free_y') #add this as an interactive graph
   
 
-sex.scale <- c("female" = "red", "male" = "blue")
+sex.scale <- c("female" = "#E24645", "male" = "#428DB6")
 
 pop %>%  
-  filter(age_range != '0 - 4' & age_range != '80 - 84' & age_range != '85+') %>%
   group_by(age_range, sex) %>% 
-  summarize(population = sum(population)) %>% 
+  summarize(population = sum(population, na.rm = TRUE)) %>% 
   mutate(prop = population / sum(population)) %>% 
-  mutate(condition = prop >= 0.504 & sex == "female" & age_range != '75 - 79') %>%
+  mutate(condition = prop >= 0.504 & sex == "female" & age_range != '75 - 79' & age_range != '80 - 84' & age_range != '85+' & age_range != '60 - 64') %>%
   mutate(condition = any(condition)) %>% 
   ggplot(aes(x = age_range, y = prop, color = sex, group = sex)) + 
-  geom_col(position = "dodge", fill = "grey60") +
+  geom_hline(yintercept = .504, color = 'grey20', linetype = 'dashed', alpha = .7) + 
+  geom_col(position = "dodge", fill = "grey73") +
   geom_col(aes(fill = sex, alpha = condition), position = "dodge") +
   scale_color_manual(values = sex.scale) +
   scale_fill_manual(values = sex.scale, guide = F) +
   scale_alpha_manual(values = c("TRUE" = 1, "FALSE" = 0)) + 
   geom_text(aes(x = age_range, y = prop, fill = sex, label = percent(prop)), position = position_dodge(width = .9), vjust = .358, hjust = 1.1,size = 4, color = 'White') +
   scale_y_continuous(limits = c(0, 1), expand = c(0,0)) + 
-  geom_hline(yintercept = .504, color = 'White') + 
   coord_flip()+
   theme_pub() +
   no_gridlines() + 
   no_x_axis() +
   no_x_line() +
+  annotate(geom = 'text', x = .6, y = .57, parse = TRUE, label = '"- Global % of " * phantom("Women")', size = 3, color = 'grey20') + 
+  annotate(geom = 'text', x = .6, y = .57, parse = TRUE, label='phantom("- Global % of ") * "Women"', size = 3, color="#E24645") + 
   no_ticks() +
+  theme(axis.text.y=element_text(color = "grey55"),
+        plot.title = element_text(color = 'grey45')) + 
   coord_flip() + 
   no_legend() + 
-  labs(x = '', y = '')
+  labs(x = '', y = '', title = 'Women significantly outnumber men from 20 to 59 in Hong Kong')  
+
+
+  
 
 
